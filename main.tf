d1ea77b0999502320b86d378b4815a13c5fecd98
Fred Nixon <john@johnfnixon.com>
2021-01-17 22:29:48 -0500

    Final typo tweeks, this will run

--- main.tf
+++ main.tf
@@ -55,18 +55,16 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-	name = "module.eks.cluster_id"
+	name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-	name = "module.eks.cluster_id"
+	name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
-	version = "~> 1.9"
-
 	host = data.aws_eks_cluster.cluster.endpoint
-	cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster.certificate_authority.0.data)
+	cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 	token = data.aws_eks_cluster_auth.cluster.token
 	load_config_file = false
 }
