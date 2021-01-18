e6670a9df6ee2cc5b9da3cf68cab407f11333fdb
Fred Nixon <john@johnfnixon.com>
2021-01-17 21:28:09 -0500

    add in VPC

--- main.tf
+++ main.tf
@@ -9,3 +9,30 @@ data "aws_availabilty_zones" "azs" {
 locals {
 	cluster_name = "eks-cci-cluster"
 }
+
+madule "vpc" {
+	source = "terraform-aws-modules/vpc/aws"
+	version = "~> 2.48"
+
+	name = "eks-cci-vpc"
+	cidr = "10.0.0.0/16"
+
+	azs	= slice(data.aws_availability_zones.azs.names, 0, 2)
+	private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+	public_subnets  = ["10.1.3.0/24", "10.1.4.0/24"]
+	enable_nat_gateway = true
+	single_nat_gateway = true
+	enable_dns_hostnames = true
+
+	tags = { "kubernetes.io/cluster/${local.cluster_name}" = "shared" }
+
+	public_subnet_tags = {
+		"kubernetes.io/cluster/${local.cluster_name}" = "shared"
+		"kubernetes.io/role/elb = "1"
+	}
+
+	private_subnet_tags = {
+		"kubernetes.io/cluster/${local.cluster_name}" = "shared"
+		"kubernetes.io/role/internal-elb = "1"
+	}
+}
